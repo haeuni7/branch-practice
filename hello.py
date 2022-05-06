7ab32c30c494afd242ab47e90a77f894261ea0dd
jjim7 <haeun705@gmail.com>
2022-05-06 15:10:24 +0900

    feat: Print yeardream when i is times of 15

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-for i in range(1,20+1):
-    if i%3==0:
-        print('year')
+for i in range(1,16+1):
+    if i%15==0:
+        print('yeardream')
+    elif i%3==0:
+        print("yaer")
     else:
         print(i)
